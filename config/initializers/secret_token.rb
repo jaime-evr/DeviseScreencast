8252ab5f3179129e5d1c6823e49bdf5a8ae7ca21
Jaime Victoria <jaime.victoria@crowdint.com>
2014-01-08 18:04:27 -0600

    Initial Commit
    - instaled devise gem
    - generated HomePage controller
    - created User model for devise
    - created links for sign in, sign up, and sign out

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DeviseScreencast::Application.config.secret_key_base = 'f1bed584cbd646a6dbd76846c6b5588a3e2d755323dfdbd7ef20cab3ad03c419d7acb8d876a7dfc3aa9b798af3bba199d4461d75eab8420676ba1702fa09c6b1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DeviseScreencast::Application.config.session_store :cookie_store, key: '_DeviseScreencast_session'
